5f296e323aef0049ef8b2736c2dde0652bad48be
SeanOmoruyi <C22346841@mytudublin.ie>
2025-02-26 11:44:43 +0000

    Update Type Tester.py

--- Type Tester.py
+++ Type Tester.py
@@ -2,3 +2,12 @@ import tkinter as tk
 from tkinter import messagebox
 import random
 import time
+
+
+TEXTS = [
+    "Artificial intelligence is the future of technology"
+    "Jesus Christ is King"
+    "If Peter Piper picked a peck of pickled peppers"
+    "Many an anemone sees an enemy anemone"
+    "Python is a versatile programming language"
+]
